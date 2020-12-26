619af3e6e90829325fbd6009ca543ad89abf9592
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-12-26 11:01:12 -0300

    feat: create array of calculator items

---
+++ src/utils/utils.tsx
@@ -0,0 +1,105 @@
+import React from 'react';
+import { ReactComponent as DeleteLogo } from '../assets/icons/delete.svg';
+
+export const calculatorItems = [
+  {
+    key: 0,
+    render: 'C',
+    value: 'C',
+  },
+  {
+    key: 1,
+    render: '+/-',
+    value: '+/-',
+  },
+  {
+    key: 2,
+    render: '%',
+    value: '%',
+  },
+  {
+    key: 3,
+    render: '/',
+    value: '/',
+  },
+  {
+    key: 4,
+    render: '7',
+    value: '7',
+  },
+  {
+    key: 5,
+    render: '8',
+    value: '8',
+  },
+  {
+    key: 6,
+    render: '9',
+    value: '9',
+  },
+  {
+    key: 7,
+    render: '*',
+    value: '*',
+  },
+  {
+    key: 8,
+    render: '4',
+    value: '4',
+  },
+  {
+    key: 9,
+    render: '5',
+    value: '5',
+  },
+  {
+    key: 10,
+    render: '6',
+    value: '6',
+  },
+  {
+    key: 11,
+    render: '-',
+    value: '-',
+  },
+  {
+    key: 12,
+    render: '1',
+    value: '1',
+  },
+  {
+    key: 13,
+    render: '2',
+    value: '2',
+  },
+  {
+    key: 14,
+    render: '3',
+    value: '3',
+  },
+  {
+    key: 15,
+    render: '+',
+    value: '+',
+  },
+  {
+    key: 16,
+    render: '0',
+    value: '0',
+  },
+  {
+    key: 17,
+    render: '.',
+    value: '.',
+  },
+  {
+    key: 18,
+    render: <DeleteLogo />,
+    value: 'R',
+  },
+  {
+    key: 19,
+    render: '=',
+    value: '=',
+  },
+];
