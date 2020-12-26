1252d92a6d16ef620261072e21ef7b4cbf4f2049
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-12-26 17:36:53 -0300

    feat: add theme to application

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Helmet } from 'react-helmet';
-
+import { ThemeProvider } from 'styled-components';
 import Routes from './routes';
 import GlobalStyle from './assets/styles/global';
+import { AppContext } from './hooks/AppContext';
 
-const App: React.FC = () => (
-  <>
-    <Helmet titleTemplate="App | %s" defaultTitle="App" />
-    <Routes />
-    <GlobalStyle />
-  </>
-);
+const App: React.FC = () => {
+  const { theme } = useContext(AppContext);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Helmet titleTemplate="App | %s" defaultTitle="App" />
+      <Routes />
+      <GlobalStyle />
+    </ThemeProvider>
+  );
+};
 
 export default App;
