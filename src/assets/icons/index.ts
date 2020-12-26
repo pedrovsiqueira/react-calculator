20996aa73342fc2c0740aca01a6225533d281a84
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-12-26 18:41:05 -0300

    refactor: add index for icons and add new icons

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,19 @@
+import DeleteDark from './delete-dark.svg';
+import DeleteLight from './delete-light.svg';
+import BatteryDark from './battery-dark.svg';
+import BatteryLight from './battery-light.svg';
+import SignalDark from './signal-dark.svg';
+import SignalLight from './signal-light.svg';
+import DarkMode from './dark-mode.svg';
+import LightMode from './light-mode.svg';
+
+export {
+  DeleteDark,
+  DeleteLight,
+  BatteryDark,
+  BatteryLight,
+  SignalDark,
+  SignalLight,
+  DarkMode,
+  LightMode,
+};
