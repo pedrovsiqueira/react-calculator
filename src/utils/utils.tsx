242410e83072bddd8ba81f7790cac9b00c20520c
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-12-26 17:34:47 -0300

    feat: create usePersistedState utils

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -1,5 +1,27 @@
-import React from 'react';
-import { ReactComponent as DeleteLogo } from '../assets/icons/delete.svg';
+import React, { useState, useEffect, Dispatch, SetStateAction } from 'react';
+import { ReactComponent as DeleteLogo } from '../assets/icons/delete-dark.svg';
+
+type Response<T> = [T, Dispatch<SetStateAction<T>>];
+
+export function usePersistedState<T>(
+  key: string,
+  initialState: T,
+): Response<T> {
+  const [state, setState] = useState(() => {
+    const storageValue = localStorage.getItem(key);
+
+    if (storageValue) {
+      return JSON.parse(storageValue);
+    }
+    return initialState;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(state));
+  }, [key, state]);
+
+  return [state, setState];
+}
 
 export const calculatorItems = [
   {
