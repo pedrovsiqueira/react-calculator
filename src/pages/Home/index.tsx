e6b9e5bf79bdd58063ed5ebeb5934176b806348d
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-12-26 11:23:07 -0300

    feat: add logic to home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,73 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+import * as math from 'mathjs';
 import { Helmet } from 'react-helmet';
+import { Header, Button } from '../../components';
+import { Container, StyledBackground, ButtonsContainer } from './styles';
+import { calculatorItems } from '../../utils/utils';
 
-import { Container } from './styles';
-import { Background } from '../../components';
+const Home: React.FC = props => {
+  const [input, setInput] = useState('');
+  const [result, setResult] = useState('');
+  const elements = ['+', '-', '/', '%', '*'];
+  const lastElement = input.charAt(input.length - 1);
 
-const Home: React.FC = () => {
+  useEffect(() => {
+    return () => {
+      setInput('');
+      setResult('');
+    };
+  }, []);
+
+  const handleCalculate = useCallback(() => {
+    const isLastInputAnElement = elements.includes(lastElement);
+
+    if (isLastInputAnElement) {
+      return setResult(math.evaluate(input.slice(0, -1)));
+    }
+
+    return setResult(math.evaluate(input));
+  }, [input, elements, lastElement]);
+
+  const addToInput = useCallback(
+    (value: string) => {
+      if (elements.includes(lastElement) && elements.includes(value)) {
+        return null;
+      }
+
+      switch (value) {
+        case '=':
+          handleCalculate();
+          break;
+        case 'C':
+          setResult('');
+          setInput('');
+          break;
+        case 'R':
+          setInput(prevState => prevState.slice(0, -1));
+          setResult('');
+          break;
+        default:
+          setInput(prevState => prevState + value);
+      }
+    },
+    [handleCalculate, elements, lastElement],
+  );
+
   return (
     <>
       <Helmet title="Home" />
       <Container>
-        <Background />
+        <StyledBackground {...props}>
+          <Header result={result} input={input} />
+
+          <ButtonsContainer>
+            {calculatorItems.map(item => (
+              <Button key={item.key} onClick={() => addToInput(item.value)}>
+                {item.render}
+              </Button>
+            ))}
+          </ButtonsContainer>
+        </StyledBackground>
       </Container>
     </>
   );
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -6,3 +6,24 @@ export const Container = styled.div`
   align-items: center;
   height: 100vh;
 `;
+
+export const StyledBackground = styled.div`
+  width: 199px;
+  height: 398px;
+  border-radius: 20px;
+  background-color: #374353;
+
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ButtonsContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  margin-left: 14px;
+
+  button {
+    margin: 14px 14px 0 0;
+  }
+`;
