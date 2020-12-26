0c2a03d0d73d40b3e3a7138fcc72ac8e13ce986b
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-12-26 11:04:26 -0300

    feat: create global styles

---
+++ src/assets/styles/global.ts
@@ -0,0 +1,42 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 16px;
+    font-family: 'Lato'
+  }
+
+  html, body, #root {
+    width: 100vw;
+    max-width: 100%;
+    min-height: 100vh;
+  }
+
+  body, input, textarea, button {
+    font: 1rem 400 sans-serif;
+    color: #333;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  ul {
+    padding: 0;
+    list-style: none;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  button {
+    cursor: pointer;
+    border: none;
+    outline: none;
+  }
+`;
